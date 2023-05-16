7a844ac1d416ca9618320f770e5297a21dea2a6f
Vini Patel <vinip@uci.edu>
2023-05-16 13:30:09 -0700

    Index.py completed

--- index.py
+++ index.py
@@ -1,11 +1,9 @@
 import os
 from pathlib import Path
 import json
-import io
-import pickle
 from bs4 import BeautifulSoup
 import re
-from collections import Counter, defaultdict
+from collections import Counter
 from nltk import stem
 from urllib.parse import urldefrag
 import sys
@@ -13,11 +11,10 @@ from math import log10
 import time
 
 STEMMER = stem.PorterStemmer()
-PATH = "ANALYST/"
-partialIndexPath = "partial_index_"
+PATH = "DEV/"
 fullIndexPath = "index.jsonl"
-importantTags = ["h1", "h2", "h3", "strong", "b"]
-batch_size = 1024 * 1024 * 1024
+importantTags = ["h1", "h2", "h3", "h4", "strong", "b"]
+batch_size = 3000000
 
 
 class Index:
@@ -25,36 +22,32 @@ class Index:
         self.filepath = path
         self.idToUrl = {}
         self.urlToId = {}
-        self.inverted_index = defaultdict(list)
-        self.batchCounter = 0
+        self.inverted_index = {}
         self.numberOfFilesProcessed = 0
         self.document_number = 0
         self.numberOfTokensProcessed = 0
         self.partialIndexFiles = []
         self.partial_file_index = 0
 
-    def generate_line(self, filename, chunk_size=1024 * 1024):
-        """This function reads file in chunks (1GB). It returns a single key and posting pair."""
+    def generate_line(self, filename):
+        """This function reads file. It returns a single key and posting pair."""
         # Used BufferReader because this allows us to read the data in chunks without reading line by line and could be efficient for very large files
-        with open(filename, "rb") as file:
-            buffer = io.BufferedReader(file, chunk_size)
-            while True:
-                chunk = buffer.readline()
-                if not chunk:
-                    break
-                line = chunk.decode().strip()
+        with open(filename) as f:
+            for line in f:
                 yield json.loads(line)
 
-    def write_to_pickle(self):
-        """Writing the data to pickle file."""
-        # We have used pickle here because writing to pickle file would be much more efficient than writing to json file.
-        with open("idToUrl.pickle", "wb") as f:
-            buffer = io.BufferedWriter(f)
-            pickle.dump(self.idToUrl, buffer)
-            buffer.flush()
+    def write_to_json(self):
+        """Writing the data to json file."""
+        with open("idToUrl.json", "w") as f:
+            jsonObj = json.dumps(self.idToUrl)
+            f.write(jsonObj)
 
-    def tfidf_score(self):
-        pass
+    def tfidf_score(self, postingsList: list):
+        frequency_tfidf = list()
+        for post in postingsList:
+            tfidf = round((1 + log10(post[1])) * (log10(self.numberOfFilesProcessed / len(postingsList))), 2)
+            frequency_tfidf.append((post[0], tfidf))
+        return frequency_tfidf
 
     def build_index(self):
         path = Path(self.filepath)
@@ -67,79 +60,116 @@ class Index:
                 if not file.is_file():
                     continue
 
-                self.process_file(file)
-                self.document_number += 1
+                for line in self.generate_line(file):
+                    file_dict = line
+                    url = urldefrag(file_dict["url"]).url
 
+                    if url in self.urlToId:
+                        continue
+                    self.urlToId[url] = self.document_number
+                    self.idToUrl[self.document_number] = url
+
+                    raw_content = file_dict["content"]
+                    soup = BeautifulSoup(raw_content, features = "html.parser")
+                    
+                    important_words = set()
+                    for tags in soup.find_all(importantTags):
+                        for words in re.sub(r"[^a-zA-Z0-9\s]", "", tags.text.lower()).split():
+                            important_words.add(STEMMER.stem(words))
+                    
+                    soup = soup.find_all()
+                    frequency = Counter()
+                    for line in soup:
+                        if not line.text:
+                            continue
+                        tokens = [STEMMER.stem(word) for word in re.sub(r"[^a-zA-Z0-9\s]", "", line.text.lower()).split()]
+                        frequency.update(tokens)
+
+                    for token, frequency in frequency.items():
+                        if token not in self.inverted_index:
+                            self.inverted_index[token] = []
+
+                        if token in important_words:
+                            self.inverted_index[token].append((self.document_number, frequency * 100))
+                        else:
+                            self.inverted_index[token].append((self.document_number, frequency))
+                self.numberOfFilesProcessed += 1
+                self.document_number += 1
+                
                 if sys.getsizeof(self.inverted_index) >= batch_size:
                     self.partial_index()
 
         if self.inverted_index:
             self.partial_index()
 
-    def process_file(self, file):
-        try:
-            with open(file) as jsonFile:
-                file_dict = json.load(jsonFile)
-                url = urldefrag(file_dict["url"]).url
-
-                self.urlToId[url] = self.document_number
-                self.idToUrl[self.document_number] = url
-
-                raw_content = file_dict["content"]
-                soup = BeautifulSoup(raw_content, "html.parser")
-                important_words = self.extract_important_words(soup)
-                token_frequency = self.tokenize_content(soup)
-
-                for token, frequency in token_frequency.items():
-                    if token not in self.inverted_index:
-                        self.inverted_index[token] = []
-
-                    if token in important_words:
-                        self.inverted_index[token].append((self.document_number, frequency * 100))
-                    else:
-                        self.inverted_index[token].append((self.document_number, frequency))
-                self.numberOfFilesProcessed += 1
-        except Exception as e:
-            print(f"Error processing file {file}: {e}")
-            return None
-
-        return file
-
-    def extract_important_words(self, content):
-        important_words = set()
-        for tags in content.find_all(importantTags):
-            for words in re.sub(r"[^a-zA-Z0-9\s]", "", tags.text.lower()).split():
-                important_words.add(STEMMER.stem(words))
-        return important_words
-
-    def tokenize_content(self, content):
-        content = content.find_all()
-        frequency = Counter()
-        for line in content:
-            if not line.text:
-                continue
-            tokens = [STEMMER.stem(word) for word in re.sub(r"[^a-zA-Z0-9\s]", "", line.text.lower()).split()]
-            frequency.update(tokens)
-        return frequency
 
     def partial_index(self):
-        with open(f"partial_index_{self.partial_file_index}.pickle", "wb") as partialFile:
+        path = f"partial_index_{self.partial_file_index}.jsonl"
+        with open(path, "w") as partialFile:
             sorted_index = sorted(self.inverted_index.items(), key=lambda x: x[0])
             self.inverted_index = {}
             for key, value in sorted_index:
-                keyVal = pickle.dumps({key: value})
+                keyVal = json.dumps({key: value})
                 partialFile.write(keyVal + "\n")
-            self.partial_file_index += 1
-            self.partialIndexFiles.append(f"partial_index_{self.partial_file_index}.pickle")
+        self.partial_file_index += 1
+        self.partialIndexFiles.append(path)
 
-    def merge_index(self):
-        pass
+    def merge_files(self):
+        """ opens main index and all partial indices, sorts and writes all partial indices into main """
 
-    def printStats(self):
-        # totalSize = os.stat("index.jsonl").st_size
+        with open(fullIndexPath, 'w') as final:  # open final index
+            file_generators = [self.generate_line(filename) for filename in self.partialIndexFiles]  # generator obj for each partial
+            next_words = []  # stores most recent word yielded from each generator
+            toBeRemoved = set()  # stores empty generators queued for removal
+            index_list = []
+
+            for i, gen in enumerate(file_generators):
+                currGensNext = list(next(gen).items())
+                next_words.append((currGensNext[0][0], currGensNext[0][1], file_generators[i]))
+
+            while file_generators:  # loop until all generators empty
+                next_words.sort(key=lambda x: x[0])  # sort yielded words
+                getNextVals = [next_words[0][2]]  # list of generators that need to yield new value
+                postings = next_words[0][1]  # initialize postings of min nextWord, may merge if there is a match later
+                i = 1
+                while i < len(next_words):
+                    if next_words[i][0] == next_words[i - 1][0]:
+                        postings.extend(next_words[i][1])  # same word, merge postings
+                        getNextVals.append(next_words[i][2])
+                    else:
+                        break
+                    i += 1
+
+                self.numberOfTokensProcessed += 1
+                postings.sort(key=lambda x: x[0])  # sort postings by docid
+                postings = self.tfidf_score(postings)  # change raw frequency to tf-idf
+                keyValAsJson = {next_words[i-1][0]: postings}  # write k,v pair into final index
+                index_list.append(keyValAsJson)
+                next_words = next_words[len(getNextVals):]  # remove written word
+
+                for i, gen in enumerate(getNextVals):
+                    try:
+                        currGensNext = list(next(gen).items())
+                        next_words.append((currGensNext[0][0], currGensNext[0][1], gen))
+                    except StopIteration:
+                        toBeRemoved.add(gen)
+                file_generators = [gen for gen in file_generators if gen not in toBeRemoved]
+                toBeRemoved = set()
+
+            json.dump(index_list, final)  # write the list of JSON objects to the file
+            final.write("\n")  # add a newline character to the end of the file
+
+        for file in self.partialIndexFiles:  # remove partial indices
+            Path(file).unlink()
+            # unindent onces???
+
+
+    
+    def print_stats(self):
+        totalSize = os.stat("index.jsonl").st_size
         print("Number of files processed:", self.numberOfFilesProcessed)
         print("Number of unique tokens:", self.numberOfTokensProcessed)
-        print("Total disk size (bytes):")
+        print("Total disk size (bytes):", totalSize)
 
 
 if __name__ == "__main__":
@@ -147,7 +177,8 @@ if __name__ == "__main__":
     print(startTime)
     index = Index(PATH)
     index.build_index()
-    index.write_to_pickle()
-    index.printStats()
+    index.write_to_json()
+    index.merge_files()
+    index.print_stats()
     endTime = time.strftime("%H:%M:%S")
     print(endTime)

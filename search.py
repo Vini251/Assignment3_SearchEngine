8920d9cf2d7a8a383a2198a3b2366a9089ee56ea
Vini Patel <80379653+Vini251@users.noreply.github.com>
2023-05-25 22:18:13 -0700

    code updated for m3

--- search.py
+++ search.py
@@ -1,77 +1,113 @@
 import json
-from nltk import stem
 import re
-import tkinter as tk
-import time
 import math
+import time
+import tkinter as tk
+from tkinter import messagebox
+from collections import defaultdict
+from nltk import stem
+from scipy.spatial.distance import cosine
+import numpy as np
 
 STEMMER = stem.PorterStemmer()
 
-def calculate_cosine_similarity(query_vector, document_vector):
-    dot_product = sum(query_vector[token] * document_vector.get(token, 0) for token in query_vector)
-    query_norm = math.sqrt(sum(value * value for value in query_vector.values()))
-    document_norm = math.sqrt(sum(value * value for value in document_vector.values()))
-    similarity = dot_product / (query_norm * document_norm) if query_norm * document_norm != 0 else 0
-    return similarity
-
-
-def get_similar_docs(query):
-    # Load the index file
+def load_index():
+    index_data = {}
     with open('index.jsonl', 'r') as file:
-        index_data = file.readlines()
+        for line in file:
+            data = json.loads(line)
+            index_data.update(data)
+    return index_data
 
+
+def load_idToUrl():
     # Load the idToUrl mapping file
     with open('idToUrl.json', 'r') as id_file:
         id_to_url = json.load(id_file)
+    return id_to_url
 
-    # Initialize a dictionary to store document numbers and their matching score
-    similar_docs = {}
-
-    # Split the query into individual tokens
+def process_query(query, index_data, id_to_url):
     query_tokens = [STEMMER.stem(word) for word in re.sub(r"[^a-zA-Z0-9\s]", "", query.lower()).split()]
+    #print(query_tokens)
+    query_tokens, query_vector = mod_query_vector(query_tokens)
+    #print(query_tokens, query_vector)
+    relevant_indexes = {key: value for key, value in [retrieve_index(word, index_data) for word in query_tokens]}
+    #print(relevant_indexes)
+    vectors = create_doc_tfidf_matrix(query_tokens, relevant_indexes)
+    #print(vectors)
+    vectors, avg_max = get_best_quartile(vectors)
+    # print(vectors)
+    # print(avg_max)
+    query_vector = normalize(query_vector)
+    #print(query_vector)
+    normed = {document: normalize(vectors[document]) for document in vectors}
+    #print(normed)
+    cosine_rank = cosine_ranking(query_vector, normed)
+    #print(cosine_rank)
 
-    # Iterate over each line in the index file
-    for line in index_data:
-        # Parse the JSON object
-        data = json.loads(line)
-
-        # Create a dictionary to store the document vector
-        doc_vector = {token: tfidf for token, tfidf in data.items() if token in query_tokens}
-
-        # Calculate the cosine similarity between the query vector and the document vector
-        similarity = calculate_cosine_similarity(query_vector, doc_vector)
-        
-        # Iterate over each token and its corresponding document numbers
-        for token, doc_scores in data.items():
-            if token in query_tokens:
-                # Iterate over each document number and its corresponding TF-IDF score
-                for doc_score in doc_scores:
-                    doc_number = doc_score[0]
-                    tfidf_score = doc_score[1]
-
-                    # Add the document number and score to the dictionary
-                    if doc_number in similar_docs:
-                        similar_docs[doc_number] += tfidf_score
-                    else:
-                        similar_docs[doc_number] = tfidf_score
-
-    # Sort the similar_docs dictionary based on the cumulative TF-IDF scores
-    sorted_docs = sorted(similar_docs.items(), key=lambda x: x[1], reverse=True)
-
-    # Extract the document numbers from the sorted list
-    similar_doc_numbers = [doc_number for doc_number, _ in sorted_docs]
-
-    # Retrieve the corresponding URLs from the idToUrl mapping
-    similar_doc_urls = [id_to_url.get(str(doc_number)) for doc_number in similar_doc_numbers]
-
-    return similar_doc_urls
+    rankings = {}
+    for doc in cosine_rank:
+        if len(query_tokens) < 3:
+            rankings[doc] = np.sum(vectors[doc])
+        else:
+            rankings[doc] = cosine_rank[doc] * 0.6 + 0.4 * np.mean(vectors[doc]) / avg_max
+    best = sorted(rankings, key=lambda x: -rankings[x])
+    #print(best)
+    top_urls = [id_to_url[str(doc)] for doc in best]
+    return top_urls[:min(len(top_urls), 5)]
 
 
-def search():
+def cosine_ranking(query_vector: dict, vector: dict):
+    return {document: np.nansum(query_vector * vector[document]) for document in vector}
+
+def normalize(vector):
+    vector = np.array(vector, dtype=float)
+    length = np.nansum(vector ** 2) ** 0.5
+    vector = vector / length
+    return vector
+
+def get_best_quartile(vector):
+    sum_vector = {doc: np.mean(np.array(vector[doc])) for doc in vector}
+    best = sorted(sum_vector, key=lambda x: -sum_vector[x])
+    extract = math.floor(len(sum_vector) / 4) if math.floor(len(sum_vector) / 4) >= 10 else len(sum_vector)
+    if extract > 500:
+        extract = 500
+    best = best[0:extract + 1]
+    return {doc: vector[doc] for doc in best}, np.mean(vector[best[0]])
+
+
+def create_doc_tfidf_matrix(terms: list, inverted_index: dict) -> dict:
+    vector = {}  # dictionary - documents are keys, tf-idf expressed as a list initially
+    for i in range(len(terms)):
+        for document, tfidf_scores in inverted_index[terms[i]]:
+            if document in vector:
+                vector[document][i] = tfidf_scores
+            else:
+                vector[document] = [0 for _ in terms]
+                vector[document][i] = tfidf_scores
+    return vector
+
+
+def mod_query_vector(query: list) -> tuple:
+    query_set_list = list(set(query))
+    q_vect = [0 for _ in query_set_list]
+    for term in query:
+        if term in query: q_vect[query_set_list.index(term)] += 1
+        else: q_vect[query_set_list.index(term)] = 1
+    return query_set_list, np.array(q_vect)
+
+def retrieve_index(word, index_data):
+    if word in index_data:
+        return word, index_data[word]
+    raise ValueError("Word not found in the inverted index.")
+
+
+
+def perform_search():
     query = entry.get()
     if query:
         t1 = time.time()
-        similar_doc_list = get_similar_docs(query)
+        similar_doc_list = process_query(query, index_data, idToUrl_data)
         result_text.delete(1.0, tk.END)
         for i in range(min(5, len(similar_doc_list))):
             result_text.insert(tk.END, f"{similar_doc_list[i]}\n\n")
@@ -79,6 +115,12 @@ def search():
         time_label.config(text=f"{t2 - t1:.2f} seconds")
     else:
         result_text.delete(1.0, tk.END)
+
+
+index_data = load_index()
+idToUrl_data = load_idToUrl()
+
+# process_query("cristina lopes", index_data, idToUrl_data)
 
 root = tk.Tk()
 root.title("Search Engine")
@@ -92,7 +134,7 @@ label.pack(side=tk.LEFT)
 entry = tk.Entry(frame, width=40)
 entry.pack(side=tk.LEFT)
 
-search_button = tk.Button(frame, text="Search", command=search)
+search_button = tk.Button(frame, text="Search", command=perform_search)
 search_button.pack(side=tk.LEFT, padx=10)
 
 result_text = tk.Text(root, height=30, width=100)
@@ -102,5 +144,4 @@ time_label = tk.Label(root, text="")
 time_label.pack()
 
 root.mainloop()
-
 

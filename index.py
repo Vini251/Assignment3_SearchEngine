673bbc95e66c6916ea824cd489120057ebf34afc
Vini Patel <80379653+Vini251@users.noreply.github.com>
2023-05-27 15:33:17 -0700

    Changed json to csv for faster loading

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import os
-from pathlib import Path
 import json
+from pathlib import Path
+import csv
 from bs4 import BeautifulSoup
 import re
 from collections import Counter
@@ -12,9 +13,11 @@ import time
 
 STEMMER = stem.PorterStemmer()
 PATH = "DEV/"
-fullIndexPath = "index.jsonl"
+fullIndexPath = "index.csv"
 importantTags = ["h1", "h2", "h3", "strong", "b"]
 batch_size = 3000000
+# Increase the field size limit
+csv.field_size_limit(1024 * 1024 * 1024) 
 
 
 class Index:
@@ -35,18 +38,29 @@ class Index:
             for word in self.important_words:
                 file.write(word + '\n')
 
+    def write_id_to_url(self):
+        """Writing the idToUrl mapping to a CSV file."""
+        with open("idToUrl.csv", "w", newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["id", "url"])
+            for doc_id, url in self.idToUrl.items():
+                writer.writerow([doc_id, url])
+
+
     def generate_line(self, filename):
-        """This function reads file. It returns a single key and posting pair."""
-        # Used BufferReader because this allows us to read the data in chunks without reading line by line and could be efficient for very large files
+        """This function reads the file and yields each JSON object."""
         with open(filename) as f:
             for line in f:
-                yield json.loads(line)
+                obj = json.loads(line)
+                yield obj
 
-    def write_to_json(self):
-        """Writing the data to json file."""
-        with open("idToUrl.json", "w") as f:
-            jsonObj = json.dumps(self.idToUrl)
-            f.write(jsonObj)
+    def write_to_csv(self):
+        """Writing the data to a CSV file."""
+        with open("idToUrl.csv", "w", newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["id", "url"])
+            for doc_id, url in self.idToUrl.items():
+                writer.writerow([doc_id, url])
 
     def tfidf_score(self, postingsList: list):
         frequency_tfidf = list()
@@ -58,31 +72,28 @@ class Index:
     def build_index(self):
         path = Path(self.filepath)
         for directory in path.iterdir():
-            # print(directory)
             if not directory.is_dir():
                 continue
             for file in directory.iterdir():
-                # print(file)
                 if not file.is_file():
                     continue
 
-                for line in self.generate_line(file):
-                    file_dict = line
-                    url = urldefrag(file_dict["url"]).url
+                for obj in self.generate_line(file):
+                    url = urldefrag(obj["url"]).url
 
                     if url in self.urlToId:
                         continue
-                    self.urlToId[url] = self.document_number
-                    self.idToUrl[self.document_number] = url
+                    doc_id = self.document_number
+                    self.urlToId[url] = doc_id
+                    self.idToUrl[doc_id] = url
 
-                    raw_content = file_dict["content"]
-                    soup = BeautifulSoup(raw_content, features = "html.parser")
-                    
-                
+                    raw_content = obj["content"]
+                    soup = BeautifulSoup(raw_content, features="html.parser")
+
                     for tags in soup.find_all(importantTags):
                         for words in re.sub(r"[^a-zA-Z0-9\s]", "", tags.text.lower()).split():
                             self.important_words.add(STEMMER.stem(words))
-                    
+
                     soup = soup.find_all()
                     frequency = Counter()
                     for line in soup:
@@ -96,82 +107,82 @@ class Index:
                             self.inverted_index[token] = []
 
                         if token in self.important_words:
-                            self.inverted_index[token].append((self.document_number, frequency * 100))
+                            self.inverted_index[token].append((doc_id, frequency * 100))
                         else:
-                            self.inverted_index[token].append((self.document_number, frequency))
+                            self.inverted_index[token].append((doc_id, frequency))
                 self.numberOfFilesProcessed += 1
                 self.document_number += 1
-                
+
                 if sys.getsizeof(self.inverted_index) >= batch_size:
                     self.partial_index()
 
         if self.inverted_index:
             self.partial_index()
 
-
     def partial_index(self):
-        path = f"partial_index_{self.partial_file_index}.jsonl"
-        with open(path, "w") as partialFile:
+        path = f"partial_index_{self.partial_file_index}.csv"
+        with open(path, "w", newline="") as partialFile:
+            writer = csv.writer(partialFile)
             sorted_index = sorted(self.inverted_index.items(), key=lambda x: x[0])
             self.inverted_index = {}
             for key, value in sorted_index:
-                keyVal = json.dumps({key: value})
-                partialFile.write(keyVal + "\n")
+                postings = ', '.join(f"{doc_id}:{frequency}" for doc_id, frequency in value)
+                writer.writerow([key, postings])
         self.partial_file_index += 1
         self.partialIndexFiles.append(path)
 
+
     def merge_files(self):
-        """ opens main index and all partial indices, sorts and writes all partial indices into main """
+        """Opens the main index and all partial indices, sorts and writes all partial indices into the main index."""
+        with open(fullIndexPath, 'w', newline='') as final:
+            writer = csv.writer(final)
+            writer.writerow(["token", "postings"])
 
-        with open(fullIndexPath, 'w') as final:  # open final index
-            file_generators = [self.generate_line(filename) for filename in self.partialIndexFiles]  # generator obj for each partial
-            next_words = []  # stores most recent word yielded from each generator
-            toBeRemoved = set()  # stores empty generators queued for removal
+            file_readers = [csv.reader(open(filename)) for filename in self.partialIndexFiles]
+            next_rows = []
+            to_be_removed = set()
 
-            for i, gen in enumerate(file_generators):
-                currGensNext = list(next(gen).items())
-                next_words.append((currGensNext[0][0], currGensNext[0][1], file_generators[i]))
+            for i, reader in enumerate(file_readers):
+                try:
+                    curr_row = next(reader)
+                    next_rows.append((curr_row[0], curr_row[1:], reader))
+                except StopIteration:
+                    to_be_removed.add(reader)
 
-            while file_generators:  # loop until all generators empty
-                next_words.sort(key=lambda x: x[0])  # sort yielded words
-                getNextVals = [next_words[0][2]]  # list of generators that need to yield new value
-                postings = next_words[0][1]  # initialize postings of min nextWord, may merge if there is a match later
+            while file_readers:
+                next_rows.sort(key=lambda x: x[0])
+                get_next_vals = [next_rows[0][2]]
+                postings = next_rows[0][1]
                 i = 1
-                while i < len(next_words):
-                    if next_words[i][0] == next_words[i - 1][0]:
-                        postings.extend(next_words[i][1])  # same word, merge postings
-                        getNextVals.append(next_words[i][2])
+                while i < len(next_rows):
+                    if next_rows[i][0] == next_rows[i - 1][0]:
+                        postings.extend(next_rows[i][1])
+                        get_next_vals.append(next_rows[i][2])
                     else:
                         break
                     i += 1
 
                 self.numberOfTokensProcessed += 1
-                postings.sort(key=lambda x: x[0])  # sort postings by docid
-                postings = self.tfidf_score(postings)  # change raw frequency to tf-idf
-                keyValAsJson = {next_words[i-1][0]: postings}  # write k,v pair into final index
+                postings.sort(key=lambda x: int(x[0]))
+                writer.writerow([next_rows[i - 1][0]] + postings)
 
-                json.dump(keyValAsJson, final)  # write the list of JSON objects to the file
-                final.write("\n")  # add a newline character to the end of the file
+                next_rows = next_rows[len(get_next_vals):]
 
-                next_words = next_words[len(getNextVals):]  # remove written word
-
-                for i, gen in enumerate(getNextVals):
+                for reader in get_next_vals:
                     try:
-                        currGensNext = list(next(gen).items())
-                        next_words.append((currGensNext[0][0], currGensNext[0][1], gen))
+                        curr_row = next(reader)
+                        next_rows.append((curr_row[0], curr_row[1:], reader))
                     except StopIteration:
-                        toBeRemoved.add(gen)
-                file_generators = [gen for gen in file_generators if gen not in toBeRemoved]
-                toBeRemoved = set()
+                        to_be_removed.add(reader)
 
-        for file in self.partialIndexFiles:  # remove partial indices
+                file_readers = [reader for reader in file_readers if reader not in to_be_removed]
+                to_be_removed = set()
+
+        for file in self.partialIndexFiles:
             Path(file).unlink()
-            # unindent onces???
 
-
-    
     def print_stats(self):
-        totalSize = os.stat("index.jsonl").st_size
+        totalSize = os.stat(fullIndexPath).st_size
         print("Number of files processed:", self.numberOfFilesProcessed)
         print("Number of unique tokens:", self.numberOfTokensProcessed)
         print("Total disk size (bytes):", totalSize)
@@ -182,7 +193,8 @@ if __name__ == "__main__":
     print(startTime)
     index = Index(PATH)
     index.build_index()
-    index.write_to_json()
+    index.write_to_csv()
+    index.write_id_to_url()
     index.merge_files()
     index.write_important_words()
     index.print_stats()
